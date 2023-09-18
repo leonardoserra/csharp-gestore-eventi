6c24e130ce5d4bb50b4407c580034118d3e8f2a1
leonardoserra <serra.leonardo92@gmail.com>
2023-09-18 11:43:25 +0200

    fix formato DateTime -> CultureInfo

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic;
+using System.Globalization;
 
 namespace csharp_gestore_eventi
 {
@@ -12,8 +13,10 @@ namespace csharp_gestore_eventi
             string title = Console.ReadLine();
 
             Console.Write("Data evento in formato dd/MM/yyyy: ");
+            CultureInfo italianFormat = new CultureInfo("it-IT");
             string stringDate = Console.ReadLine();
-            DateTime date = DateTime.ParseExact(stringDate,"dd/MM/yyyy",null);
+            DateTime date = DateTime.ParseExact(stringDate,"dd/MM/yyyy",italianFormat);
+            //DateTime date = DateTime.ParseExact(stringDate,"dd/MM/yyyy",null);
 
             Console.WriteLine($"{title} {date.ToString("dd/MM/yyyy")}");
         }
