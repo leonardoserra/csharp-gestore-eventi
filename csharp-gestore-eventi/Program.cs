d4228e18c892900672867802be064ce21d25ad85
leonardoserra <serra.leonardo92@gmail.com>
2023-09-18 11:25:44 +0200

    problemi con conversione data su program...

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace csharp_gestore_eventi
+﻿using Microsoft.VisualBasic;
+
+namespace csharp_gestore_eventi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prova per Bryan!");
+            Console.WriteLine("Per creare un evento compili i campi richiesti:\r\n\t");
+            
+            Console.Write("Titolo evento: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Data evento in formato dd/MM/yyyy: ");
+            string stringDate = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(stringDate,"dd/MM/yyyy",null);
+
+            Console.WriteLine($"{title} {date.ToString("dd/MM/yyyy")}");
         }
     }
 }
