2261887af767b329d4345f787449171831f98629
leonardoserra <serra.leonardo92@gmail.com>
2023-09-18 15:46:08 +0200

    creata struttura program. problema sul ciclo for

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -7,11 +7,7 @@ namespace csharp_gestore_eventi
     {
         static void Main(string[] args)
         {
-
             CultureInfo italianFormat = new CultureInfo("it-IT");
-
-
-
             try
             {
                 Console.Write("Inizializzazione, che nome vuoi dare al Programma di Eventi? Digita: ");
@@ -19,56 +15,47 @@ namespace csharp_gestore_eventi
                 EventsProgram customProgram = new EventsProgram(programTitle);
                 Console.Write($"Bene! Programma {programTitle} creato! Quanti eventi andrai ad aggiungere al programma? Digita un numero: ");
                 int eventQuantity = int.Parse(Console.ReadLine());
-                bool managingEvents = true;
-                while (managingEvents)
-                {
                 List<Event> events = new List<Event>(eventQuantity);
-                for (int i = 0; i < events.Count; i++)
-                {
-                    try
+                bool managingEvents = true;
+                //while (managingEvents)
+                //{
+                    for (int i = 0; i < eventQuantity; i++)
                     {
-                        Console.Write($"Titolo evento n.{i+1}: ");
-                        string title = Console.ReadLine();
+                        try
+                        {
+                            Console.WriteLine($"Creazione evento n.{i + 1}");
+                            Console.Write($"Titolo evento n.{i+1}: ");
+                            string title = Console.ReadLine();
 
-                        Console.Write("Data evento in formato dd/MM/yyyy: ");
-                        string stringDate = Console.ReadLine();
-                        DateTime date = DateTime.ParseExact(stringDate, "dd/MM/yyyy", italianFormat);
+                            Console.Write("Data evento in formato dd/MM/yyyy: ");
+                            string stringDate = Console.ReadLine();
+                            DateTime date = DateTime.ParseExact(stringDate, "dd/MM/yyyy", italianFormat);
 
-                        Console.Write("Capacità massima di prenotazioni: ");
-                        int maxCapacity = int.Parse(Console.ReadLine());
-                        events[i] = new Event(title, date, maxCapacity);
+                            Console.Write("Capacità massima di prenotazioni: ");
+                            int maxCapacity = int.Parse(Console.ReadLine());
+                            events[i] = new Event(title, date, maxCapacity);
 
-                    }catch (Exception ex){
-                       Console.WriteLine(ex.Message);
-                       Console.WriteLine("Ri assegna i dati all evento");
-                       i--;
-                       continue;
+                        }catch (Exception ex){
+                            Console.WriteLine(ex.Message);
+                            Console.WriteLine("Ri assegna i dati all evento");
+                            i--;
+                            continue;
+                        }
+
                     }
-                }
-                Console.WriteLine();
+                    Console.WriteLine();
+                //}
                     
+                int totalEventsInActualProgram = customProgram.EventsQuantity();
+                Console.WriteLine($"Ci sono ben {totalEventsInActualProgram} eventi in programma!");
+                customProgram.ProgramDetails();
 
-
-                    Event evento1 = new Event(title, date, maxCapacity);
-
-
-                    EventsProgram programma = new EventsProgram("provaProgrammaEventi");
-                    programma.AddEvent(evento1);
-
-                    List<Event> listaFiltrata = programma.SearchByDate(stringDate);
-                    string infoProgram = EventsProgram.ListEventsInfo(programma.Events);
-                    //Console.WriteLine(infoProgram);
-                    string infoFilteredProgram = EventsProgram.ListEventsInfo(listaFiltrata);
-                    //Console.WriteLine(infoFilteredProgram);
-                    //Console.WriteLine("Numero eventi in programma:" + programma.EventsQuantity());
-                    Console.WriteLine(programma.ProgramDetails());
-
-                    programma.DismissAllEvents();
-                    //Console.WriteLine("Numero eventi in programma:" + programma.EventsQuantity());
-
-
-                    Console.WriteLine(programma.ProgramDetails());
-                }
+                Console.WriteLine("Cerca uno o piú eventi in una data specifica, digita gg/mm/aaaa: ");
+                string requestedDate = Console.ReadLine();
+                List<Event> eventsByDate = customProgram.SearchByDate(requestedDate);
+                string eventListByDate = EventsProgram.ListEventsInfo(eventsByDate);
+                Console.WriteLine(eventListByDate);
+                customProgram.DismissAllEvents();
             }
             catch (Exception ex)
             {
