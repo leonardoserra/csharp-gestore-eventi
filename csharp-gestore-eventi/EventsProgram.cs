bb6c50026eea084a9f885422c4037c2d91cd3fca
leonardoserra <serra.leonardo92@gmail.com>
2023-09-18 11:51:30 +0200

    ok costruttore EventsProgram

---
+++ csharp-gestore-eventi/EventsProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class EventsProgram
+    {
+        private string title;
+        private List<Event> events;
+
+        public EventsProgram(string title)
+        {
+            this.title = title;
+            events = new List<Event>();
+        }
+    }
+}
--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace csharp_gestore_eventi
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
