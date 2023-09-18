8fd997fae8404eea4ade6926981a6ade1c45b178
leonardoserra <serra.leonardo92@gmail.com>
2023-09-18 17:04:11 +0200

    creazione Conference class e constructor base ,override su ToString() di Conference

---
+++ csharp-gestore-eventi/Conference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{   
+    public class Conference : Event
+    {
+        private string relatorName;
+        private float price;
+        public Conference(string title, DateTime date, int maxCapacity, string relatorName, float price) : base(title, date, maxCapacity)
+        {
+            this.relatorName = relatorName;
+            this.price = price;
+        }
+
+
+        public override string ToString()
+        {
+            return $"\r\n\tData dell'evento: {this.date.ToString("dd/MM/yyyy")} - Titolo: {this.title} - Relatore: {this.relatorName} - Prezzo: {this.price.ToString("0.00")}\r\n";
+        }
+    }
+}
